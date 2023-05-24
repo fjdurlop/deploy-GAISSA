b6333115ee3514331ab2981fa3fa2df86e0d3ecd
fjdurlop <fjdurlop0@gmail.com>
2023-05-24 12:49:42 +0200

    experiments

--- scripts/bert_02.py
+++ scripts/bert_02.py
@@ -5,7 +5,53 @@ import torch
 #from models import *
 from models import LMBERTModel, Model
 
+
+def decode(tokenizer, token_ids,
+           skip_special_tokens: bool = False,
+           clean_up_tokenization_spaces: bool = None,
+           spaces_between_special_tokens: bool = True,
+           **kwargs,) -> str:
+   tokenizer._decode_use_source_tokenizer = kwargs.pop("use_source_tokenizer", False)
+
+   filtered_tokens = tokenizer.convert_ids_to_tokens(token_ids, skip_special_tokens=skip_special_tokens)
+
+   # To avoid mixing byte-level and unicode for byte-level BPT
+   # we need to build string separately for added tokens and byte-level tokens
+   # cf. https://github.com/huggingface/transformers/issues/1133
+   sub_texts = []
+   current_sub_text = []
+   for token in filtered_tokens:
+      if skip_special_tokens and token in tokenizer.all_special_ids:
+            continue
+      if token in tokenizer.added_tokens_encoder:
+            if current_sub_text:
+               sub_texts.append(tokenizer.convert_tokens_to_string(current_sub_text))
+               current_sub_text = []
+            sub_texts.append(token)
+      else:
+            current_sub_text.append(token)
+   if current_sub_text:
+      sub_texts.append(tokenizer.convert_tokens_to_string(current_sub_text))
+
+   if spaces_between_special_tokens:
+      text = " ".join(sub_texts)
+   else:
+      text = "".join(sub_texts)
+
+   clean_up_tokenization_spaces = (
+      clean_up_tokenization_spaces
+      if clean_up_tokenization_spaces is not None
+      else tokenizer.clean_up_tokenization_spaces
+   )
+   if clean_up_tokenization_spaces:
+      clean_text = tokenizer.clean_up_tokenization(text)
+      return clean_text
+   else:
+      return text
+
 testing_input = False
+
+
 
 if testing_input:
     user_text = input('Enter text with [MASK]: ')
